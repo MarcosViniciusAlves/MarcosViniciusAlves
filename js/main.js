6877fb14b5e91736c588dd3424d28ae9240b669e
Marcos Vinicius <dealvesmarcos@gmail.com>
2020-09-13 18:06:06 -0300

    commite 9

--- js/main.js
+++ js/main.js
@@ -8,17 +8,23 @@ const interesses = ["mysql","python"];
 function ocupaComPython () {
     saidaDoSelect.innerHTML = ""
 
-    saidaDoSelect.innerHTML = '<img height="100" src="css/images/python.png"><h3 id="frase">minha trajetoria com python tem muito a ver com <abbr title="Inteligência Artificial">IA</abbr> e também muito a ver com Machine Learning,gosto dessa linguagem pelo fácil uso e por ser muito presente , é interessante aprender ela<h3>'
+    saidaDoSelect.innerHTML = '<img height="100" id="imgAb" src="css/images/python.png"><h3 id="frase" class="fraseCMarg" >minha trajetoria com python tem muito a ver com <abbr title="Inteligência Artificial">IA</abbr> e também muito a ver com Machine Learning,gosto dessa linguagem pelo fácil uso e por ser muito presente , é interessante aprender ela<h3>'
 
     saidaDoSelect.innerHTML += '<br><br><br><div id="niveis" class="red">Básico</div><div id="niveis" class="red" >Intermediário</div><div id="niveis" class="red">Avançado</div>'
+
+    saidaDoSelect.innerHTML += '<h3 id="curiosidade" >Curiosidade : Uma curiosidade interessante sobre a linguagem de programação python é que a linguagem não recebeu esse nome por causa da espécie de serpente, mas sim do seriado de comédia da BBC Monty Python’s Flying Circus da qual Guido van Rossum é um fã.</h3>';
 }
+
 function ocupaComMysql () {
     saidaDoSelect.innerHTML = ""
 
-    saidaDoSelect.innerHTML = '<img height="100" src="css/images/mysql.png">'
-    
-}
+    saidaDoSelect.innerHTML = '<img height="100" id="imgAb" src="css/images/mysql.png"><h3 id="frase" class="fraseCMarg">Meus interesses com MySql são no geral para um objetivo maior , aprender php e usar todo conhecimento com as tecnologias aqui citadas para desenvolver uma aplicação,dou muito valor o sistema de gerenciamento de banco de dados , é uma das mais fáceis e também uma das mais úteis.</h3>'
 
+    saidaDoSelect.innerHTML += '<br><br><br><div id="niveis" class="red">Básico</div><div id="niveis" class="red" >Intermediário</div><div id="niveis" class="red">Avançado</div>'
+
+    saidaDoSelect.innerHTML += '<h3 id="curiosidade">Qual o motivo do golfinho?!? : A alegação feita foi de que o golfinho é um animal rápido , esperto e ágil , e seu nome é Sakila porquê em uma campanha chamada "Name the Dolphin" foi escolhido o nome do bixinho.</h3>'
+
+}
 
 function identificaSelect(){
     if(select.value === interesses[0]){
